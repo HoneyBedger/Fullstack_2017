b784d25ded7a09076b9dcd371715f2237a531f9d
Darya Komsa <komsa.darya@gmail.com>
2018-07-19 18:50:13 -0400

    http example and Express example

--- Node/node-http/index.js
+++ Node/node-http/index.js
@@ -1,16 +1,44 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  console.log(req.headers);
+  console.log(`Request for ${req.url} by method ${req.method}`);
 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<htnl><body><h1>Hello, world!</h1></body></html>');
+  if (req.method == 'GET') {
+    var fileUrl;
+    if (req.url == '/') fileUrl = '/index.html';
+    else fileUrl = req.url;
+
+    var filePath = path.resolve('./public' + fileUrl);
+    const fileExt = path.extname(filePath);
+    if (fileExt == '.html') {
+      fs.exists(filePath, (exists) => {
+        if (!exists) {
+          res.statusCode = 404;
+          res.setHeader('Content-Type', 'text/html');
+          res.end('<html><body><h1>Error 404: file ' + fileUrl + ' not found. </h1></body></html>');
+          return;
+        }
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        fs.createReadStream(filePath).pipe(res);
+      });
+    } else {
+      res.status = 404;
+      res.setHeader('Content-Type', 'text/html');
+      res.end('<html><body><h1>Error 404: ' + fileUrl + ' is not an html file. </h1></body></html>');
+    }
+  } else {
+    res.status = 404;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>Error 404: ' + req.method + ' is not GET. </h1></body></html>');
+  }
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}`);
 });
