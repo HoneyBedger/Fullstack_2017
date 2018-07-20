d53ea4379be3e9f908269d2b52f6b57868975d5d
Darya Komsa <komsa.darya@gmail.com>
2018-07-20 15:37:47 -0400

    Mongoose with nested schemas

---
+++ Node/node-mongo/index.js
@@ -0,0 +1,40 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const dboper = require('./operations');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const dbname = 'conFusion';
+
+
+MongoClient.connect(url)
+    .then((client) => {
+      console.log("Connected correctly to server");
+
+      const db = client.db(dbname);
+      dboper.insertDocument(db, {name: "Vadonut", description: "test"}, "dishes")
+      .then((result) => {
+        console.log("Insert Document:\n", result.ops);
+        return dboper.findDocuments(db, "dishes");
+      })
+      .then((docs) => {
+        console.log("Found documents:\n", docs);
+        return dboper.updateDocument(db, {name: "Vadonut"},
+          {description: "Updated test"}, "dishes");
+      })
+      .then((result) => {
+        console.log("Updated document:\n", result.result);
+        return dboper.findDocuments(db, "dishes");
+      })
+      .then((docs) => {
+        console.log("Found updated documents:\n", docs);
+        return db.dropCollection("dishes");
+      })
+      .then((result) => {
+        console.log("Dropped Collection: ", result);
+        return client.close();
+      })
+      .catch((err) => console.log(err));
+
+}, (err) => console.log(err))
+.catch((err) => console.log(err));
