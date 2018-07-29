beb27c0aaa74be29e8f6abcd2479c0b8f53dd177
Darya Komsa <komsa.darya@gmail.com>
2018-07-29 10:30:30 -0400

    Fetching json files from simple json server

--- React/confusion/src/redux/comments.js
+++ React/confusion/src/redux/comments.js
@@ -1,13 +1,20 @@
-import {COMMENTS} from '../shared/comments';
 import * as ActionTypes from './ActionTypes';
 
-export const Comments = (state = COMMENTS, action) => {
+export const Comments = (state = {
+    errMessage: null, comments: []
+  }, action) => {
   switch(action.type) {
     case ActionTypes.ADD_COMMENT:
       var comment = action.payload;
-      comment.id = state.length;
+      comment.id = state.comments.length;
       comment.date = new Date().toISOString();
-      return state.concat(comment); //.concat() is an immutable operation, so produces a new state array and adds to it
+      return {...state, comments: state.comments.concat(comment)}; //.concat() is an immutable operation, so produces a new state array and adds to it
+    case ActionTypes.ADD_COMMENTS:
+      return {...state, isLoading: false,
+        errMessage: null, comments: action.payload};
+    case ActionTypes.COMMENTS_FAILED:
+      return {...state, isLoading: false,
+        errMessage: action.payload, comments: []};
     default:
       return state;
   }
