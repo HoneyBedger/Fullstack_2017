4154dd05441c947869a8051a24a5edbf83a3dfac
Darya Komsa <komsa.darya@gmail.com>
2018-07-30 13:54:50 -0400

    Animation components

--- React/confusion/src/components/DishdetailComponent.js
+++ React/confusion/src/components/DishdetailComponent.js
@@ -5,6 +5,7 @@ import {LocalForm, Control, Errors} from 'react-redux-form';
 import {Link} from 'react-router-dom';
 import {Loading} from './LoadingComponent';
 import {baseUrl} from '../shared/baseUrl';
+import {FadeTransform, Fade, Stagger} from 'react-animation-components';
 
 const required = (value) => value && value.length;
 const minLength = (length) => (value) => (value) && (value.length >= length);
@@ -98,13 +99,17 @@ class CommentForm extends Component {
 
 function RenderDish({dish}) {
   return (
-    <Card>
-      <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name}/>
-      <CardBody>
-        <CardTitle>{dish.name}</CardTitle>
-        <CardText>{dish.description}</CardText>
-      </CardBody>
-    </Card>
+    <FadeTransform in transformProps={{
+        exitTransform: 'scale(0.5) translateY(-50%)'
+      }}>
+      <Card>
+        <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name}/>
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    </FadeTransform>
   );
 }
 
@@ -112,10 +117,12 @@ function RenderDish({dish}) {
 function RenderComments({comments, postComment, dishId}) {
   const commentList = comments.map((comment) => {
     return (
-      <li key={comment.id}>
-        <p>{comment.comment}</p>
-        <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-      </li>
+      <Fade in>
+        <li key={comment.id}>
+          <p>{comment.comment}</p>
+          <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+        </li>
+      </Fade>
     );
   });
 
@@ -124,7 +131,9 @@ function RenderComments({comments, postComment, dishId}) {
       <div>
         <h4>Comments</h4>
         <ul className="list-unstyled">
-          {commentList}
+          <Stagger in>
+            {commentList}
+          </Stagger>
         </ul>
         <CommentForm postComment={postComment} dishId={dishId}/>
       </div>
